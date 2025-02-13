85f98d3e421c26c556f22e0c9733c71dcb327deb
Lucianosc <luchotandil90@gmail.com>
2025-02-13 20:04:07 +0100

    feat(HeroSection): TsParticlesBackground

---
+++ components/HeroBackground.tsx
@@ -0,0 +1,30 @@
+"use client"; // Required for client-side interactivity in Next.js
+
+import React from "react";
+import Particles from "@tsparticles/react";
+
+import type { Container } from "@tsparticles/engine";
+import { heroBackgroundOptions } from "@/constants/tsParticlesOptions";
+import { useTSParticles } from "@/context/TSParticlesContext";
+
+const ParticlesImageMask = () => {
+  const { isEngineInit } = useTSParticles();
+
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    if (!container) throw new Error("Container not found");
+  };
+
+  return (
+    <>
+      {isEngineInit && (
+        <Particles
+          id="tsparticles"
+          particlesLoaded={particlesLoaded}
+          options={heroBackgroundOptions}
+        />
+      )}
+    </>
+  );
+};
+
+export default ParticlesImageMask;
